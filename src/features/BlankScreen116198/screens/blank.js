fdf2d23063d3d0f71d4d3370747e89021e930cd9
Crowdbotics <team@crowdbotics.com>
2020-12-24 13:59:19 +0000

    Update components

--- src/features/BlankScreen116198/screens/blank.js
+++ src/features/BlankScreen116198/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,57 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = { TextInput_2: "", TextInput_3: "", TextInput_5: "" }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <TextInput
+        placeholder="Number Input Placeholder"
+        keyboardType="numeric"
+        value={this.state.TextInput_2}
+        onChangeText={nextValue => this.setState({ TextInput_2: nextValue })}
+      />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        style={styles.TextInput_3}
+        value={this.state.TextInput_3}
+        onChangeText={nextValue => this.setState({ TextInput_3: nextValue })}
+      />
+      <Text style={styles.Text_4}>Sample text content</Text>
+      <TextInput
+        placeholder="Number Input Placeholder"
+        keyboardType="numeric"
+        style={styles.TextInput_5}
+        value={this.state.TextInput_5}
+        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
+  View_1: {},
+  TextInput_2: {},
+  TextInput_3: {
+    alignSelf: "center",
+    color: "#37c83c",
+    backgroundColor: "#f5dbdb",
+    fontFamily: "Merriweather-BoldItalic"
   },
-});
+  Text_4: { color: "#46c318" },
+  TextInput_5: { color: "#39e123", fontFamily: "Merriweather-Regular" }
+})
